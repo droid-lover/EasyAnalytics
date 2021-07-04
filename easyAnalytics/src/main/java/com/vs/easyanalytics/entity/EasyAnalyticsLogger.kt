aa2bc47ccc1c0ceacd10d5217d8922cd1cf848a4
myJarvis <ersachin20@gmail.com>
2021-07-04 14:55:52 +0530

    Added EasyAnalytics logging entity

--- easyAnalytics/src/main/java/com/vs/easyanalytics/entity/EasyAnalyticsLogger.kt
+++ easyAnalytics/src/main/java/com/vs/easyanalytics/entity/EasyAnalyticsLogger.kt
@@ -1,3 +1,13 @@
 package com.vs.easyanalytics.entity
 
-data class EasyAnalyticsLogger(val name:String, val dataConsumed:String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class EasyAnalyticsLogger( @ColumnInfo val name:String,  @ColumnInfo val dataConsumed:String , @ColumnInfo val time:String)
+{
+    @PrimaryKey(autoGenerate = true)
+    var logId: Int = 0
+}
+
